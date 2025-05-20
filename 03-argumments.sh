198dd2037d8c3133e1797a1a509f56c818f67639
venu <venubonkuri1234@gmail.com>
2025-05-20 10:03:57 +0530

    hello world

---
+++ 03-argumments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+# these $1 and $2 are like arguments then we can give commands from outside
+echo "$PERSON1 : hey $PERSON2 how are you"
+echo "$PERSON2: iam fine modiji, what about you"
+echo "$PERSON1 : iam also fine"
+echo "$PERSON2 : how's going war with pakistan"
+echo "$PERSON1 : its going good"
+echo "$PERSON2 : ok, bye"
+echo "$PERSON1 : bye"
