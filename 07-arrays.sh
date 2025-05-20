22af90cd10aa00b840b4a1327697a75df220a4f7
venu <venubonkuri1234@gmail.com>
2025-05-20 10:46:25 +0530

    hello world

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,3 +3,12 @@
 MOVIES=("Inception" "Interstellar" "The Dark Knight" "Memento")
 
 echo "my favourite movie among them is: ${MOVIES[2]}"
+
+echo "most logical and thinking movie of christopher nolan is: ${MOVIES[0]}"
+
+echo "most fanbase for a movie: ${MOVIES[1]}"
+
+echo "best visuals movie is: ${MOVIES[2]}"
+
+echo "top 4 movies in hollywood are: ${MOVIES[@]}"
+
