86a7ae0aa7b8595a469d0173077e5313cbabaadc
venu <venubonkuri1234@gmail.com>
2025-05-20 09:46:37 +0530

    hello world

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "modi : hey venu how are you"
+echo "venu : iam fine modiji, what about you"
+echo "modi : iam also fine"
+echo "venu : how's going war with pakistan"
+echo "modi : its going good"
+echo "venu : ok, bye"
+echo "modi : bye"
