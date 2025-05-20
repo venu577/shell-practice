06f5bf55875c3f20c3beb92a400563b2c6bbe6a6
venu <venubonkuri1234@gmail.com>
2025-05-20 09:57:44 +0530

    hello world

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "modi : hey venu how are you"
-echo "venu : iam fine modiji, what about you"
-echo "modi : iam also fine"
-echo "venu : how's going war with pakistan"
-echo "modi : its going good"
-echo "venu : ok, bye"
-echo "modi : bye"
+PERSON1=MODI
+PERSON2=VENU
+
+echo "$PERSON1 : hey $PERSON2 how are you"
+echo "$PERSON2: iam fine modiji, what about you"
+echo "$PERSON1 : iam also fine"
+echo "$PERSON2 : how's going war with pakistan"
+echo "$PERSON1 : its going good"
+echo "$PERSON2 : ok, bye"
+echo "$PERSON1 : bye"
