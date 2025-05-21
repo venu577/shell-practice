d39a6a56e5b22fcd4045a47347e3746dd6db59b0
venu <venubonkuri1234@gmail.com>
2025-05-21 09:14:59 +0530

    hello world

--- 10-installcommands.sh
+++ 10-installcommands.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 #checks if user have root access or not if user has id 0 then user is root other than 0 user don't have root access
  
- if [ USERID -ne 0 ]
+ if [ $USERID -ne 0 ]
     then
        echo "error: please run with root access"
        exit 1
