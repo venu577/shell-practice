2eaf08f976d91f3bb8b76e92c8a48b9dd56524d5
venu <venubonkuri1234@gmail.com>
2025-05-21 08:06:17 +0530

    hello world

--- 08-specialvars.sh
+++ 08-specialvars.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "all variables passed to the script: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running the script: $USER"
+echo "home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last background process: $!"
