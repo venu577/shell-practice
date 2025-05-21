9f977c5855f10b6c5675bce8d4167fb621d5df78
venu <venubonkuri1234@gmail.com>
2025-05-21 09:14:09 +0530

    hello world

--- 10-installcommands.sh
+++ 10-installcommands.sh
@@ -3,18 +3,19 @@
 USERID=$(id -u)
 #checks if user have root access or not if user has id 0 then user is root other than 0 user don't have root access
  
- if [ $USERID -ne 0 ]
+ if [ USERID -ne 0 ]
     then
        echo "error: please run with root access"
        exit 1
      else
         echo "you are running with root access"
 
-    fi      
-dnf install mysql -y
+    fi 
+
+  dnf install mysql -y
 #we don't know if mysql is installed properly or not so we check the exit status of the last command
 
-if [ $? -eq 0 ]
+ if [ $? -eq 0 ]
    then 
       echo "installing mysql is successfull"
    else 
