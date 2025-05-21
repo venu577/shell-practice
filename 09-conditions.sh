e4164741612967acefa8597bc6bc228f1a8d559d
venu <venubonkuri1234@gmail.com>
2025-05-21 08:17:51 +0530

    hello world

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-NUMBER=$!
-if [ $NUMBER: -lt: 10 ]
+NUMBER=$1
+# check if the number is lessthan or not lessthan 10
+if [ $NUMBER -lt 10 ]
 
 then
-echo "given number $NUMBER is less than 10"
+    echo "given number $NUMBER is less than 10"
 
 else
-echo "given number $NUMBER is greater than 10"
+    echo "given number $NUMBER is not less than 10"
 
 fi 
