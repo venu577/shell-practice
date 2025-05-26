77a885e8a331107fd3862cea2339ee8c6cb09658
venu <venubonkuri1234@gmail.com>
2025-05-26 08:49:20 +0530

    colors created

---
+++ 12-colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "\e[31m hello world \e[0m"
+echo -e "\e[32m hello world \e[0m"
+echo -e "\e[33m hello world \e[0m"
+echo -e "\e[34m hello world \e[0m"
+echo -e "\e[35m hello world \e[0m"
+echo -e "\e[36m hello world \e[0m"
+
