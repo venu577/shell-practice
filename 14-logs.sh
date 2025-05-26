f29cd94606505b1d729f6c57e6384c3775bcaedd
venu <venubonkuri1234@gmail.com>
2025-05-26 13:01:34 +0530

    logs

---
+++ 14-logs.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+USERID=$(id -u)
+# Check if the script is run with root access
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+#colours R is for error G is for success Y is for installation N is for normal text
+
+LOGS_FOLDER="/var/log/shellscript-logs" 
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# Extract the script name without the extension
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+# Define the log file path
+# Create the logs folder if it doesn't exist
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+
+then
+    echo -e "$R error: run with root access $N" &>>$LOG_FILE
+    exit 1
+    else 
+    echo -e "$G you are running with root access $N" &>>$LOG_FILE
+    fi
+
+    VALIDATE(){
+        if [ $1 -eq 0 ]
+        then 
+         echo -e "installing $2 is $G successfull $N" &>>$LOG_FILE
+         else 
+         echo -e "installing $2 is $R not successfull $N" &>>$LOG_FILE
+         exit 1
+         fi
+        }
+
+dnf list installed mysql
+ if [ $? -ne 0 ]
+   then 
+   echo "mysql is not installed going to install it" &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "mysql"
+     else 
+     echo -e "mysql is $Y already installed $N nothing to do" &>>$LOG_FILE
+     fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+  then
+    echo "python3 is not installed going to install it" &>>$LOG_FILE
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+     else 
+     echo -e "python3 is $Y already installed $N nothing to do" &>>$LOG_FILE
+       fi
+
+ dnf list installed nginx
+ if [ $? -ne 0 ]
+  then 
+   echo "nginx is not installed going to install it" &>>$LOG_FILE
+   dnf install nginx -y &>>$LOG_FILE
+   VALIDATE $? "nginx"
+    else
+    echo -e "nginx is $Y already installed $N nothing to do" &>>$LOG_FILE
+    fi       
+
+      
