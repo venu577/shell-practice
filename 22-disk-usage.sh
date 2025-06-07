371c9b822e464524391c11cfdf79cd4433efb349
venu <venubonkuri1234@gmail.com>
2025-06-07 20:49:28 +0530

    deleting old logs

---
+++ 22-disk-usage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v filesystem)
+DISK_THRESHOLD=1 #In projects it will be 75
+MSG=""
+
+while IFS= read line
+do
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG="high disk usage on $PARTITION: $USAGE"
+    fi
+
+done <<< $DISK_USAGE echo -e $MSG     
