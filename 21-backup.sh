30642fa5b6167c3ac7887a27a2d5b780e4cd593a
venu <venubonkuri1234@gmail.com>
2025-06-07 20:21:20 +0530

    deleting old logs

--- 21-backup.sh
+++ 21-backup.sh
@@ -7,7 +7,7 @@ DAYS=${3:-14} # Default to 14 days if not provided
 
 LOGS_FOLDER="/var/log/shellscript-logs" 
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/backup.log" #here i changed $SCRIPT_NAME to backup.log cause i want to run this script by using command
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
