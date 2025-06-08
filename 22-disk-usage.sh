2d931c224df30b82136b3d80dd8690665b6ab373
venu <venubonkuri1234@gmail.com>
2025-06-08 15:29:29 +0530

    deleting old logs

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -v filesystem)
-DISK_THRESHOLD=1 #In projects it will be 75
+CPU_USAGE=$(top | grep -v filesystem)
+CPU_THRESHOLD=1 #In projects it will be 75
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
-    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7F}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
+    cpu_usage=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
+    PARTITION=$(echo $line | awk '{print $9F}')
+    if [ $cpu_usage -ge $CPU_THRESHOLD ]
     then
-        MSG+="high disk usage on $PARTITION: $USAGE % <br>"
+        MSG+="high CPU usage on $PARTITION: $cpu_usage % <br>"
     fi
 
 done <<< $DISK_USAGE
     echo -e $MSG
-sh mail.sh "devops team" "high disk usage" "$IP" "$MSG" "venubonkuri1234@gmail.com" "alert disk usage"
+sh mail.sh "devops team" "high cpu usage" "$IP" "$MSG" "venubonkuri1234@gmail.com" "alert disk usage"
