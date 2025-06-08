2a31c5efa343b0cda2e27ed28e3f12abe17e81c9
venu <venubonkuri1234@gmail.com>
2025-06-08 15:45:08 +0530

    deleting old logs

--- 22-disk-usage.sh
+++ 22-disk-usage.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-CPU_USAGE=$(top | grep -v filesystem)
-CPU_THRESHOLD=1 #In projects it will be 75
+DISK_USAGE=$(df -hT | grep -v filesystem)
+DISK_THRESHOLD=1 #In projects it will be 75
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
-    USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
-    PARTITION=$(echo $line | awk '{print $9F}')
-    if [ $USAGE -ge $CPU_THRESHOLD ]
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="high CPU usage on $PARTITION: $USAGE % <br>"
+        MSG+="high disk usage on $PARTITION: $USAGE % <br>"
     fi
 
-done <<< $CPU_USAGE
+done <<< $DISK_USAGE
     echo -e $MSG
-sh mail.sh "devops team" "high cpu usage" "$IP" "$MSG" "venubonkuri1234@gmail.com" "alert disk usage"
+sh mail.sh "devops team" "high disk usage" "$IP" "$MSG" "venubonkuri1234@gmail.com" "alert disk usage"
--- 23-cpu-usage.sh
+++ 23-cpu-usage.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-while true; do
-  cpu_usage=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
-  echo "CPU Usage: ${cpu_usage}"
-  sleep 1
-done
 
+CPU_USAGE=$(top)
 
+CPU_THRESHOLD=1 #In projects it will be 75
+MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+while IFS= read line
+do
+    USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
+    PARTITION=$(echo $line | awk '{print $7F}')
+    if [ $USAGE -ge $CPU_THRESHOLD ]
+    then
+        MSG+="high cpu usage on $PARTITION: $USAGE % <br>"
+    fi
+
+done <<< $CPU_USAGE
+    echo -e $MSG
+sh mail.sh "devops team" "high cpu usage" "$IP" "$MSG" "venubonkuri1234@gmail.com" "alert cpu usage"
