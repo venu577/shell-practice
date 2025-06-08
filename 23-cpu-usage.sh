0e2d47c020b670102bb77a34eb18bd8bad0ae358
venu <venubonkuri1234@gmail.com>
2025-06-08 16:02:27 +0530

    deleting old logs

--- 23-cpu-usage.sh
+++ 23-cpu-usage.sh
@@ -9,7 +9,6 @@ IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read line
 do
     USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
-    PARTITION=$(echo $line | awk '{print $7F}')
     if [ $USAGE -ge $CPU_THRESHOLD ]
     then
         MSG+="high cpu usage on $PARTITION: $USAGE % <br>"
@@ -17,4 +16,4 @@ do
 
 done <<< $CPU_USAGE
   echo -e $MSG
-  sh mail.sh "devops team" "high cpu usage" "$IP" "$MSG" "venubonkuri1234@gmail.com" "alert cpu usage"
+  sh mail.sh "devops team" "high cpu usage" $IP $MSG "venubonkuri1234@gmail.com" "alert cpu usage"
